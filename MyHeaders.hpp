8e0a06356f5ffaa89c4d25d6830917ae67ae3739
ivansnow02 <ivansnow02@gmail.com>
2024-09-09 10:08:39 +0800

    up

--- MyHeaders.hpp
+++ MyHeaders.hpp
@@ -94,3 +94,18 @@ struct TreeNode {
   TreeNode(int x, TreeNode *left, TreeNode *right)
       : val(x), left(left), right(right) {}
 };
+
+class Node {
+public:
+  int val;
+  std::vector<Node *> children;
+
+  Node() {}
+
+  Node(int _val) { val = _val; }
+
+  Node(int _val, std::vector<Node *> _children) {
+    val = _val;
+    children = _children;
+  }
+};
