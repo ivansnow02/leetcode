ce7d2603bd4af2687186bfc40eeb15656e365dcb
ivansnow02 <ivansnow02@gmail.com>
2024-09-17 10:59:45 +0800

    up

---
+++ 112.路径总和.cpp
@@ -0,0 +1,111 @@
+#include "MyHeaders.hpp"
+#include <cstddef>
+#include <memory>
+using namespace std;
+/*
+ * @lc app=leetcode.cn id=112 lang=cpp
+ *
+ * [112] 路径总和
+ *
+ * https://leetcode.cn/problems/path-sum/description/
+ *
+ * algorithms
+ * Easy (54.62%)
+ * Likes:    1389
+ * Dislikes: 0
+ * Total Accepted:    726.4K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '[5,4,8,11,null,13,4,7,2,null,null,null,1]\n22'
+ *
+ * 给你二叉树的根节点 root 和一个表示目标和的整数 targetSum 。判断该树中是否存在
+ * 根节点到叶子节点 的路径，这条路径上所有节点值相加等于目标和 targetSum
+ * 。如果存在，返回 true ；否则，返回 false 。
+ *
+ * 叶子节点 是指没有子节点的节点。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+ * 输出：true
+ * 解释：等于目标和的根节点到叶节点路径如上图所示。
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：root = [1,2,3], targetSum = 5
+ * 输出：false
+ * 解释：树中存在两条根节点到叶子节点的路径：
+ * (1 --> 2): 和为 3
+ * (1 --> 3): 和为 4
+ * 不存在 sum = 5 的根节点到叶子节点的路径。
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：root = [], targetSum = 0
+ * 输出：false
+ * 解释：由于树是空的，所以不存在根节点到叶子节点的路径。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 树中节点的数目在范围 [0, 5000] 内
+ * -1000 <= Node.val <= 1000
+ * -1000 <= targetSum <= 1000
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+private:
+  struct SNode {
+    TreeNode *node;
+    int sum;
+  };
+
+public:
+  bool hasPathSum(TreeNode *root, int targetSum) {
+    if (root == nullptr)
+      return false;
+    stack<shared_ptr<SNode>> st;
+    st.emplace(make_shared<SNode>(SNode{root, root->val}));
+    while (!st.empty()) {
+      auto cur = st.top();
+      st.pop();
+      if (cur == nullptr)
+        continue;
+      if (cur->sum == targetSum && cur->node->left == nullptr &&
+          cur->node->right == nullptr) {
+        return true;
+      }
+      if (cur->node->left)
+        st.emplace(make_shared<SNode>(
+            SNode{cur->node->left, cur->sum + cur->node->left->val}));
+      if (cur->node->right)
+        st.emplace(make_shared<SNode>(
+            SNode{cur->node->right, cur->sum + cur->node->right->val}));
+    }
+    return false;
+  }
+};
+// @lc code=end
