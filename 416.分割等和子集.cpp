f63b2d5cae209596a8f07bba3a44b5d831317b70
ivansnow02 <ivansnow02@gmail.com>
2024-10-03 14:32:37 +0800

    up

---
+++ 416.分割等和子集.cpp
@@ -0,0 +1,69 @@
+#include <vector>
+#include "MyHeaders.hpp"
+using namespace std;
+/*
+ * @lc app=leetcode.cn id=416 lang=cpp
+ *
+ * [416] 分割等和子集
+ *
+ * https://leetcode.cn/problems/partition-equal-subset-sum/description/
+ *
+ * algorithms
+ * Medium (52.69%)
+ * Likes:    2159
+ * Dislikes: 0
+ * Total Accepted:    620.6K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[1,5,11,5]'
+ *
+ * 给你一个 只包含正整数 的 非空 数组 nums
+ * 。请你判断是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：nums = [1,5,11,5]
+ * 输出：true
+ * 解释：数组可以分割成 [1, 5, 5] 和 [11] 。
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：nums = [1,2,3,5]
+ * 输出：false
+ * 解释：数组不能分割成两个元素和相等的子集。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1
+ * 1
+ *
+ *
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool canPartition(vector<int> &nums) {
+        auto sum = 0;
+        for (const auto &x: nums) {
+            sum += x;
+        }
+        if (sum % 2 == 1)
+            return false;
+        vector<int> dp(sum / 2 + 1, 0);
+        for (auto i = 0; i < nums.size(); i++) {
+            for (auto j = sum / 2; j >= nums[i]; j--) {
+                dp[j] = max(dp[j], dp[j - nums[i]] + nums[i]);
+            }
+        }
+        return dp[sum / 2] == sum / 2;
+    }
+};
+// @lc code=end
