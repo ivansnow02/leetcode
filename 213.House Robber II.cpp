40b5c0bcd0a1aee859e297b79edaed870f5a7459
ivansnow02 <ivansnow02@gmail.com>
2024-10-12 10:25:02 +0800

    up

---
+++ 213.House Robber II.cpp
@@ -0,0 +1,122 @@
+/*
+ * @lc app=leetcode.cn id=213 lang=cpp
+ * @lcpr version=20001
+ *
+ * [213] 打家劫舍 II
+ *
+ * https://leetcode.cn/problems/house-robber-ii/description/
+ *
+ * algorithms
+ * Medium (45.33%)
+ * Likes:    1630
+ * Dislikes: 0
+ * Total Accepted:    453K
+ * Total Submissions: 999.4K
+ * Testcase Example:  '[2,3,2]'
+ *
+ * 你是一个专业的小偷，计划偷窃沿街的房屋，每间房内都藏有一定的现金。这个地方所有的房屋都 围成一圈
+ * ，这意味着第一个房屋和最后一个房屋是紧挨着的。同时，相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警
+ * 。
+ *
+ * 给定一个代表每个房屋存放金额的非负整数数组，计算你 在不触动警报装置的情况下 ，今晚能够偷窃到的最高金额。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [2,3,2]
+ * 输出：3
+ * 解释：你不能先偷窃 1 号房屋（金额 = 2），然后偷窃 3 号房屋（金额 = 2）, 因为他们是相邻的。
+ *
+ *
+ * 示例 2：
+ *
+ * 输入：nums = [1,2,3,1]
+ * 输出：4
+ * 解释：你可以先偷窃 1 号房屋（金额 = 1），然后偷窃 3 号房屋（金额 = 3）。
+ * 偷窃到的最高金额 = 1 + 3 = 4 。
+ *
+ * 示例 3：
+ *
+ * 输入：nums = [1,2,3]
+ * 输出：3
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= nums.length <= 100
+ * 0 <= nums[i] <= 1000
+ *
+ *
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    int rob(vector<int> &nums) {
+        if (nums.size() == 1) {
+            return nums[0];
+        }
+        if (nums.size() == 2) {
+            return max(nums[0], nums[1]);
+        }
+        vector<int> dp(nums.size() + 1, 0);
+
+        auto robByRange = [&](int r, int l) -> int {
+            fill(dp.begin(), dp.end(), 0);
+
+
+            dp[r] = nums[r];
+            dp[r+1] = max(nums[r], nums[r + 1]);
+
+            for (auto i = r + 2; i <= l; i++) {
+                dp[i] = max(dp[i - 2] + nums[i], dp[i - 1]);
+
+            }
+
+            return dp[l];
+        };
+
+        return max(robByRange(0, nums.size()-2), robByRange(1, nums.size()-1));
+
+    }
+};
+// @lc code=end
+
+
+/*
+// @lcpr case=start
+// [2,3,2]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2,3,1]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2,3]\n
+// @lcpr case=end
+
+ */
