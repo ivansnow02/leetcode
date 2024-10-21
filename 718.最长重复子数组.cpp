28c00f4431e1b24f1892dca4fd4b3357cb8d761e
ivansnow02 <ivansnow02@gmail.com>
2024-10-21 10:08:12 +0800

    up

---
+++ 718.最长重复子数组.cpp
@@ -0,0 +1,99 @@
+/*
+ * @lc app=leetcode.cn id=718 lang=cpp
+ * @lcpr version=20002
+ *
+ * [718] 最长重复子数组
+ *
+ * https://leetcode.cn/problems/maximum-length-of-repeated-subarray/description/
+ *
+ * algorithms
+ * Medium (56.76%)
+ * Likes:    1124
+ * Dislikes: 0
+ * Total Accepted:    272.7K
+ * Total Submissions: 480.4K
+ * Testcase Example:  '[1,2,3,2,1]\n[3,2,1,4,7]'
+ *
+ * 给两个整数数组 nums1 和 nums2 ，返回 两个数组中 公共的 、长度最长的子数组的长度 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：nums1 = [1,2,3,2,1], nums2 = [3,2,1,4,7]
+ * 输出：3
+ * 解释：长度最长的公共子数组是 [3,2,1] 。
+ *
+ *
+ * 示例 2：
+ *
+ * 输入：nums1 = [0,0,0,0,0], nums2 = [0,0,0,0,0]
+ * 输出：5
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= nums1.length, nums2.length <= 1000
+ * 0 <= nums1[i], nums2[i] <= 100
+ *
+ *
+ */
+
+
+// @lcpr-template-start
+#include <limits>
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    int findLength(vector<int> &nums1, vector<int> &nums2) {
+        // dp[i][j] ：以下标i - 1为结尾的A，和以下标j - 1为结尾的B，最长重复子数组长度为dp [i][j]。
+        // （特别注意：“以下标i -1为结尾的A” 标明一定是 以A[i - 1] 为结尾的字符串 ）
+        vector<vector<int>> dp(
+                        nums1.size() + 1, vector<int>(nums2.size() + 1));
+        auto result = numeric_limits<int>::min();
+
+        for (int i = 1; i <= nums1.size(); i++) {
+            for (int j = 1; j <= nums2.size(); j++) {
+                if (nums1[i - 1] == nums2[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                result = max(result, dp[i][j]);
+
+            }
+        }
+        return result;
+    }
+
+};
+// @lc code=end
+
+
+/*
+// @lcpr case=start
+// [1,2,3,2,1]\n[3,2,1,4,7]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [0,0,0,0,0]\n[0,0,0,0,0]\n
+// @lcpr case=end
+
+ */
