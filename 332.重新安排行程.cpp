2ab8a0bd8cccf32b5bc30c5bc79408b584e442f3
ivansnow02 <ivansnow02@gmail.com>
2024-11-24 11:32:43 +0800

    up

---
+++ 332.重新安排行程.cpp
@@ -0,0 +1,137 @@
+/*
+ * @lc app=leetcode.cn id=332 lang=cpp
+ * @lcpr version=20003
+ *
+ * [332] 重新安排行程
+ *
+ * https://leetcode.cn/problems/reconstruct-itinerary/description/
+ *
+ * algorithms
+ * Hard (44.51%)
+ * Likes:    947
+ * Dislikes: 0
+ * Total Accepted:    116.7K
+ * Total Submissions: 262.2K
+ * Testcase Example:  '[["MUC","LHR"],["JFK","MUC"],["SFO","SJC"],["LHR","SFO"]]'
+ *
+ * 给你一份航线列表 tickets ，其中 tickets[i] = [fromi, toi]
+ * 表示飞机出发和降落的机场地点。请你对该行程进行重新规划排序。
+ *
+ * 所有这些机票都属于一个从 JFK（肯尼迪国际机场）出发的先生，所以该行程必须从 JFK
+ * 开始。如果存在多种有效的行程，请你按字典排序返回最小的行程组合。
+ *
+ *
+ * 例如，行程 ["JFK", "LGA"] 与 ["JFK", "LGB"] 相比就更小，排序更靠前。
+ *
+ *
+ * 假定所有机票至少存在一种合理的行程。且所有的机票 必须都用一次 且 只能用一次。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：tickets = [["MUC","LHR"],["JFK","MUC"],["SFO","SJC"],["LHR","SFO"]]
+ * 输出：["JFK","MUC","LHR","SFO","SJC"]
+ *
+ *
+ * 示例 2：
+ *
+ * 输入：tickets =
+ * [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
+ * 输出：["JFK","ATL","JFK","SFO","ATL","SFO"]
+ * 解释：另一种有效的行程是 ["JFK","SFO","ATL","JFK","ATL","SFO"] ，但是它字典排序更大更靠后。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= tickets.length <= 300
+ * tickets[i].length == 2
+ * fromi.length == 3
+ * toi.length == 3
+ * fromi 和 toi 由大写英文字母组成
+ * fromi != toi
+ *
+ *
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+#include <map>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+private:
+    // <from, <to, count>>
+    // if count > 0, can fly
+    unordered_map<string, map<string, int>> targets;
+    vector<string> result;
+    int FLIGHTNUM;
+
+    bool backTrack() {
+        if (result.size() == FLIGHTNUM) {
+            return true;
+        }
+
+        for (auto &target: targets[result.back()]) {
+            if (target.second <= 0) {
+                continue;
+            }
+            result.emplace_back(target.first);
+            target.second--;
+            if (backTrack()) {
+                return true;
+            }
+            target.second++;
+            result.pop_back();
+        }
+        return false;
+    }
+
+
+public:
+    vector<string> findItinerary(vector<vector<string>> &tickets) {
+        for (const auto &ticket: tickets) {
+            targets[ticket[0]][ticket[1]] ++;
+        }
+        FLIGHTNUM = tickets.size() + 1;
+
+        result.emplace_back("JFK");
+
+        backTrack();
+
+        return result;
+
+    }
+};
+// @lc code=end
+
+
+/*
+// @lcpr case=start
+// [["MUC","LHR"],["JFK","MUC"],["SFO","SJC"],["LHR","SFO"]]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]\n
+// @lcpr case=end
+
+ */
