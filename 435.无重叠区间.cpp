2d582d55ddbdf80e2570f65a5b7f6f941399057e
ivansnow02 <ivansnow02@gmail.com>
2024-12-30 15:20:08 +0800

    up

---
+++ 435.无重叠区间.cpp
@@ -0,0 +1,119 @@
+/*
+ * @lc app=leetcode.cn id=435 lang=cpp
+ * @lcpr version=20004
+ *
+ * [435] 无重叠区间
+ *
+ * https://leetcode.cn/problems/non-overlapping-intervals/description/
+ *
+ * algorithms
+ * Medium (52.37%)
+ * Likes:    1190
+ * Dislikes: 0
+ * Total Accepted:    304.9K
+ * Total Submissions: 582.3K
+ * Testcase Example:  '[[1,2],[2,3],[3,4],[1,3]]'
+ *
+ * 给定一个区间的集合 intervals ，其中 intervals[i] = [starti, endi] 。返回
+ * 需要移除区间的最小数量，使剩余区间互不重叠 。
+ *
+ * 注意 只在一点上接触的区间是 不重叠的。例如 [1, 2] 和 [2, 3] 是不重叠的。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ * 输入: intervals = [[1,2],[2,3],[3,4],[1,3]]
+ * 输出: 1
+ * 解释: 移除 [1,3] 后，剩下的区间没有重叠。
+ *
+ *
+ * 示例 2:
+ *
+ * 输入: intervals = [ [1,2], [1,2], [1,2] ]
+ * 输出: 2
+ * 解释: 你需要移除两个 [1,2] 来使剩下的区间没有重叠。
+ *
+ *
+ * 示例 3:
+ *
+ * 输入: intervals = [ [1,2], [2,3] ]
+ * 输出: 0
+ * 解释: 你不需要移除任何区间，因为它们已经是无重叠的了。
+ *
+ *
+ *
+ *
+ * 提示:
+ *
+ *
+ * 1 <= intervals.length <= 10^5
+ * intervals[i].length == 2
+ * -5 * 10^4 <= starti < endi <= 5 * 10^4
+ *
+ *
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+private:
+    auto inIntervals(const vector<int> &a, const int &r) {
+        return (a[0] < r) && (a[1] >= r);
+    }
+public:
+
+    int eraseOverlapIntervals(vector<vector<int>> &intervals) {
+        ranges::sort(intervals, [](auto a, auto b) { return a[1] < b[1]; });
+
+        int cnt = 0;
+        int r = -1;
+
+        for (const auto &interval: intervals) {
+            if (inIntervals(interval, r)) {
+                continue;
+            }
+            cnt++;
+            r = interval[1];
+        }
+
+        return intervals.size() - cnt;
+
+    }
+
+};
+// @lc code=end
+
+
+/*
+// @lcpr case=start
+// [[1,2],[2,3],[3,4],[1,3]]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [ [1,2], [1,2], [1,2] ]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [ [1,2], [2,3] ]\n
+// @lcpr case=end
+
+ */
